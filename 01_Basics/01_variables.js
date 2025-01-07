5174b12b8fb6167f6bada9a4cb00a6b873adc308
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-07 16:07:47 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=155223
+let accountMail="abhishekkannam@gmail.com"
+var accountPassword=123456789
+accountCity="Asangaon"
+
+accountId=23
+accountMail="abhishek123@gmail.com"
+accountPassword=123456
+accountCity="Mumbai"
+
+console.table([accountId,accountMail,accountPassword,accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using node js not browser
+
+let name="abhishek"
+let age=23
+let isLoggedIn=false
+
+//number =>2 to power53
+//bigint 
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undefined 
+//symbol =>uniquenes
+
+//------non primitive
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
