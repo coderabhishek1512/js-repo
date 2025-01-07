093fc1cbc8c0505dad5cf8943faa30d4762ff74e
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-07 16:32:40 +0000

    03_conversionOperations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score=null;
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>Number
+//"33abc"=>NaN
+//true=1;false=0
+
+let isLoggedIn=1
+let isBooleanLoggedIn=Boolean(isBooleanLoggedIn);
+
+console.log(isBooleanLoggedIn)
+
+//true=>1; false => 0
+// "" => false; "xyz" => true
