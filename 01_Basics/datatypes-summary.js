720c001aa5bf83a053711db337cccdaa06a166d0
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-08 12:10:06 +0000

    data type&comparison

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+//Primary
+// 7 Types :String,Number,Null,Undefined,Boolean,Symbol,BigInt
+const name="abhishek"
+const age=23
+const salary=null
+let company;
+let isLoggedIn=false
+
+//Reference (Non Primitive)
+//Array,Object,Function
+const arr=["Iron Man","Ant-Man","Spider-Man","Hulk"]
+const myFunction=function(){
+   console.log("Abhishek")
+}
+let myObj ={
+    name:"abhishek",
+    age:23,
+
+}
