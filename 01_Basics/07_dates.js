8454b3b44065ad45c20044b30f0f6842b98b327d
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-08 15:23:23 +0000

    dates

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,14 @@
+//Dates 
+
+let myDate=new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+let myCreatedDate= new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString())
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime)
+console.log(Math.floor(Date.now()/1000))
