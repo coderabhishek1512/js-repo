4410374dd0b0643d852996e594ff1ecd053ab498
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-08 13:20:19 +0000

    string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,26 @@
+// const name="abhishek"
+// const repoCount=50;
+//console.log(name + repoCount + " Number")
+
+//console.log(`Hello my Name is ${name} and my repo count is ${repoCount} `)
+
+const gameName= new String('abhishek')
+// console.log(gameName)
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('A'))
+
+// const newString=gameName.substring(0,4)  //substring cannot take negative value
+// console.log(newString)
+
+// const another=gameName.slice(-8,8)
+// console.log(another)
+
+const newString ="   Abhishek   "
+console.log(newString)
+console.log(newString.trim())
