a97f5a2434c9f4b17f09887f9bd26f0db399f590
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-09 10:56:50 +0000

    objects

---
+++ 02_Basics/04_object.js
@@ -0,0 +1,34 @@
+//const tinderUser=new Object()
+
+const tinderUser={}
+
+tinderUser.id="123abc"
+tinderUser.name="Abhi"
+tinderUser.isLoggedIn=false
+
+//console.log(tinderUser)
+
+const regularUser={
+    email:"abhishekkannam47@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Abhishek",
+            lastname:"Kannam"
+
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj3=Object.assign(obj1,obj2)
+//const obj3={...obj1,...obj2}
+// console.log(obj3)//
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
