ff9a71ed93423ce9cf716fd52476f1683592e95a
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-13 06:06:27 +0000

    call stack and control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+// const temp=59
+// if(temp>5){
+//     console.log("Temprature is greater than 50");
+    
+// }else{
+// console.log("Not Greater than 50");
+// }
+
+
+// const score =19
+// if (score>100) {
+//     const power="fly"
+//     console.log(`User Power:${power}`);
+    
+// } else {
+//     const power="no power"
+//     console.log(`User Power:${power}`);
+    
+// }
+
+const balance=1200
+if (balance<500) {
+    console.log("balance less than 500");
+    
+}else if(balance<750){
+console.log("balance less than 750");
+
+} 
+else if(balance<1000){
+console.log("balance less than 1000");
+
+} 
+else if(balance<1200){
+console.log("balance less than 1200");
+
+} 
+else {
+    console.log("Server Error");
+    
+}
