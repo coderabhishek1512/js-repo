a869353a2b17d1d215d7976e8b1742b00f29f794
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-13 07:19:00 +0000

    loops

---
+++ 05_iteration/two.js
@@ -0,0 +1,18 @@
+// let i=0
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i++
+// }
+
+
+// let marvel=['Hulk',"Iron Man","Thor"]
+// for (let i = 0; i < marvel.length; i++) {
+//     const element = marvel[i];
+//     console.log(element);
+// }
+
+let score=1
+do {
+    console.log(score);
+    score++
+} while (score<=10);
