57c14932f88342d74b5b64de788472ccfb4ddc0a
coderabhishek1512 <88979103+coderabhishek1512@users.noreply.github.com>
2025-01-13 06:40:42 +0000

    iteration

---
+++ 05_iteration/one.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i <=10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Outer loop");
+//     console.log(`${i} Value of Outer loop`);
+    
+//    for (let j= 0; j < 10; j++) {
+   
+//    console.log(`${i} * ${j}= ${i*j}`);
+   
+//    }
+// }
